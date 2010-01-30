1a1ff1466e2df10a6d1515e73c9aac5346f0aab6
SkewMatrix <SkewMatrix@c67418bc-a6eb-11de-8ba5-91705c1537ed>
2010-01-30 18:41:50 +0000

    Adding wire sphere convenience routine. Add comments to osgwTools/Shapes.h to describe functioning of the shape convenience routines.

--- trunk/include/osgwTools/Shapes.h
+++ trunk/include/osgwTools/Shapes.h
@@ -29,10 +29,56 @@
 namespace osgwTools {
 
 
+// This is a set of convenience routines for configuring a Geometry to render
+// specific shapes. It is intended as a replacement for OSG's ShapeDrawables.
+// While it lacks the implicit metadata found in ShapeDrawables, it allows direct
+// access to vertices and vertex data, thus moe widely supported than ShapeDrawables.
+//
+// All convenience routines follow a common convention of allowint the app to
+// supply a Geometry to use. By default, the convenience routines instantiate a
+// Geometry. All routines set a color array with a single white color.
+//
+// All shapes are centered on the origin. Use OSG Transform Nodes to translate
+// or orient the shapes.
+
+
+// Sphere generation routines
+
+// Creates a geodesic sphere starting from the 20 triangles of an icosehedron.
+// Each subdivision splits a triangle into four new triangles. The generated
+// Geometry uses a single DrawElementsUShort GL_TRIANGLES PrimitiveSet, and contains
+// vertex, normal, texture coordinate, and color data. The texture coordinates are
+// configured for unit 0 and suitable for cube mapping.
 OSGWTOOLS_EXPORT osg::Geometry* makeGeodesicSphere( const float radius=1., const unsigned int subdivisions=2, osg::Geometry* geometry=NULL );
+
+// Creates an alt-az sphere (also commonly called a lat-long sphere) using
+// GL_TRIANGLE_STRIP PrimitiveSets. subLat indicates the number of GL_TRIANGLE_STRIPS
+// wrapping the sphere in the xy plane. Each strip contains (subLong*2) triangles.
+// Normal and texture coordinate (unit 0) data are created. Texture coordinates are
+// suitable for applying a geographic projection texture. (t coordinate ranges from 0
+// at the (-z) sphere bottom to 1 at the (+z) sphere top, and s wraps around the sphere
+// with 0 at +x, to +y, to -x, to -y, and ending with 1.0 at +x.)
 OSGWTOOLS_EXPORT osg::Geometry* makeAltAzSphere( const float radius=1., const unsigned int subLat=8, const unsigned int subLong=16, osg::Geometry* geometry=NULL );
 
+// Creates an alt-az sphere using GL_LINE_LOOP and GL_LINE_STRIP PrimitiveSets.
+// No normal or texture coordinate data. StateSet is configured to disable GL_LIGHTING
+// for OpenGL1/2 builds of OSG.
+OSGWTOOLS_EXPORT osg::Geometry* makeWireAltAzSphere( const float radius=1., const unsigned int subLat=8, const unsigned int subLong=16, osg::Geometry* geometry=NULL );
+
+
+// Box generation routines
+
+// Makes an axis-aligned box using GL_TRIANGLE_STRIP PrimitiveSets. Dimensions
+// are (halfExtents*2). By default, two triangles are on each side of the box, for
+// a total of 12 triangles. However, this can be increased with the subdivisions
+// parameter, which specifices how to subdivide the faces of the sphere in x, y, and z.
+// Normal and texture coordinate (unit 0) data are created. The texture coordinate data
+// is suitable for applying an entire texture map to each face of the box.
 OSGWTOOLS_EXPORT osg::Geometry* makeBox( const osg::Vec3& halfExtents, const osg::Vec3s& subdivisions=osg::Vec3s(1,1,1), osg::Geometry* geometry=NULL );
+
+// Creates a bos using GL_LINE_LOOP and GL_LINES PrimitiveSets. No normal or
+// texture coordinate data. StateSet is configured to disable GL_LIGHTING for
+// OpenGL1/2 builds of OSG.
 OSGWTOOLS_EXPORT osg::Geometry* makeWireBox( const osg::Vec3& halfExtents, osg::Geometry* geometry=NULL );
 
 
