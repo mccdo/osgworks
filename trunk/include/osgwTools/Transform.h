f216285c8af1d4f039de704e5e7db3412d0d742a
SkewMatrix@gmail.com <SkewMatrix@gmail.com@c67418bc-a6eb-11de-8ba5-91705c1537ed>
2010-09-24 21:40:48 +0000

    Adding new utility routines to transform OSG BoundingSphere and BoundingBox classes. Adding test for same.

---
+++ trunk/include/osgwTools/Transform.h
@@ -0,0 +1,47 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * osgWorks is (C) Copyright 2009 by Kenneth Mark Bryden
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef __OSGWTOOLS_TRANSFORM_H__
+#define __OSGWTOOLS_TRANSFORM_H__1
+
+
+#include "osgwTools/Export.h"
+
+#include <osg/BoundingSphere>
+#include <osg/BoundingBox>
+#include <osg/Matrix>
+
+
+namespace osgwTools
+{
+
+
+// Transform a bounding sphere or box by a matrix, and return a copy
+// of the transformed sphere or matrix.
+OSGWTOOLS_EXPORT osg::BoundingSphere transform( const osg::Matrix& m, const osg::BoundingSphere& sphere );
+OSGWTOOLS_EXPORT osg::BoundingBox transform( const osg::Matrix& m, const osg::BoundingBox& box );
+
+
+
+// osgwTools
+}
+
+// __OSGWTOOLS_GRID_H__
+#endif
