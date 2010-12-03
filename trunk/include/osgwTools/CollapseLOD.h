c1e847c9882c2daebba11fccedf5db2d3eb986c9
deedrem@gmail.com <deedrem@gmail.com@c67418bc-a6eb-11de-8ba5-91705c1537ed>
2010-12-03 02:19:37 +0000

    Minor documentation tweak.

--- trunk/include/osgwTools/CollapseLOD.h
+++ trunk/include/osgwTools/CollapseLOD.h
@@ -128,10 +128,12 @@ after traversal returns, the application should call the \ref finishProcessingLO
 This NodeVisitor has two modes of operation. It can replace LOD Nodes with Group Nodes, or it
 can simply remove the LOD Nodes altogether. In either case, only the highest LOD child, as
 determined by the NodeSelectorCallback, remains (either attached to the new Group, or attached
-to the removed LOD's former parent). See \ref CollapseMode to control the mode of operation. COLLAPSE_TO_GROUP is useful when the
-LOD node contains callbacks, UserData, DescriptionLists, etc., as this information is
+to the removed LOD's former parent). 
+
+See \ref CollapseMode to control the mode of operation. COLLAPSE_TO_GROUP is useful when the
+LOD node contains callbacks, UserData, DescriptionLists, etc. This information is
 copied from the LOD to the new Group. COLLAPSE_TO_PARENT (the default) removes the LOD and
-the references to any such associated data.
+the references to any such associated data. */
 
 \test testvisitors
 
