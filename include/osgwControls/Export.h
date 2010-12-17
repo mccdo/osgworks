d2a1d989e323f8ab51976c0e8a0ec5e712a48605
SkewMatrix@gmail.com <SkewMatrix@gmail.com@c67418bc-a6eb-11de-8ba5-91705c1537ed>
2010-12-17 22:21:28 +0000

    Static linking support.

--- include/osgwControls/Export.h
+++ include/osgwControls/Export.h
@@ -22,14 +22,16 @@
 #define __OSGWCONTROLS_EXPORT__ 1
 
 
-#if defined ( _MSC_VER ) || defined ( __CYGWIN__ ) || defined ( __MINGW32__ ) || defined ( __BCPLUSPLUS__ ) || defined ( __MWERKS__ )
- #if defined ( OSGWCONTROLS_LIBRARY )
-  #define OSGWCONTROLS_EXPORT __declspec( dllexport )
- #else
-  #define OSGWCONTROLS_EXPORT __declspec( dllimport )
- #endif
+#if defined( _MSC_VER ) || defined( __CYGWIN__ ) || defined( __MINGW32__ ) || defined( __BCPLUSPLUS__ ) || defined( __MWERKS__ )
+    #if defined( OSGWORKS_STATIC )
+        #define OSGWCONTROLS_EXPORT
+    #elif defined( OSGWCONTROLS_LIBRARY )
+        #define OSGWCONTROLS_EXPORT __declspec( dllexport )
+    #else
+        #define OSGWCONTROLS_EXPORT __declspec( dllimport )
+    #endif
 #else
- #define OSGWCONTROLS_EXPORT
+    #define OSGWCONTROLS_EXPORT
 #endif
 
 
