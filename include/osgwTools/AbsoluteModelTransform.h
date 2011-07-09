11edf807a14fd18933e5ad8aa528142dbf09e5f4
SkewMatrix <SkewMatrix@c67418bc-a6eb-11de-8ba5-91705c1537ed>
2011-07-09 16:01:40 +0000

    Documentation review and cleanup.

--- include/osgwTools/AbsoluteModelTransform.h
+++ include/osgwTools/AbsoluteModelTransform.h
@@ -66,29 +66,34 @@ See the \ref Plugins module description.
 
 
 
-/** \brief Sets the model part of the OpenGL modelview matrix.
+/** \class AbsoluteModelTransform AbsoluteModelTransform.h <osgwTools/AbsoluteModelTransform.h>
+\brief Sets the model part of the OpenGL modelview matrix.
 
-AbsoluteModelTransform is an OSG Transform that overloads ABSOLUTE_RF to
-preserve the view. Regardless of (non-Camera) transforms above this Node in
-the hierarchy, the effective transform is concatenated
-with the view. This allows Bullet to drive a model transform with a single
-matrix.
+AbsoluteModelTransform (AMT) is an OSG Transform that overloads ABSOLUTE_RF to
+preserve the view. Non-Camera transforms above this Node in the hierarchy are
+ignored, and the matrix stored in this Node is the only transform concatenated
+with the view.
+
+This is useful, for example, when allowing a 3rd party physics simultator to
+transform parts of your scene graph without reparenting those parts to the
+scene graph root. In fact, this class was created to support the osgBullet
+project (osgBullet.googlecode.com).
 
 You can disable this behavior by setting the reference frame to RELATIVE_RF,
 which causes this Transform to behave like a regular MatrixTransform. The
 default reference frame is ABSOLUTE_RF.
 
 For the AbsoluteModelTransform to operate, it needs the current view matrix.
-The code supports obtaining the view matrix in two ways that the
-setting of OSGWORKS_SCENEVIEW_ANAGLYPHIC_STEREO_SUPPORT in CMake control. By default,
+The code supports obtaining the view matrix in two ways. Select the algorithm
+using the OSGWORKS_SCENEVIEW_ANAGLYPHIC_STEREO_SUPPORT CMake variable. By default,
 OSGWORKS_SCENEVIEW_ANAGLYPHIC_STEREO_SUPPORT is disabled, and AbsoluteModelTransform
 assumes there is a Camera in the CullVisitor NodePath that contains the view matrix.
-This is the most efficient method, but it doesn't work in some corner-case usages, such
+This is the most efficient method, but it doesn't work in some usages, such
 as when the user has enabled anaglyphic stereo via the OSG_STEREO environment variable.
 Set OSGWORKS_SCENEVIEW_ANAGLYPHIC_STEREO_SUPPORT on in CMake to support this usage. When
 enabled, AbsoluteModelTransform obtains the view matrix by accumulating the model transform
 from the NodePath, inverting it, and multiplying it by the CullVisitor current modelview
-matrix. This is a more general purpose, but less efficient solution.
+matrix. The result is the view transform. This is a more general purpose, but less efficient solution.
 
 \test amt
 
