9524b3a776cd98a4e682696c52744b0d108c2e30
SkewMatrix@gmail.com <SkewMatrix@gmail.com@c67418bc-a6eb-11de-8ba5-91705c1537ed>
2011-09-08 15:31:46 +0000

    Adding blit framebuffer support.

--- include/osgwTools/FBOUtils.h
+++ include/osgwTools/FBOUtils.h
@@ -41,12 +41,20 @@ OSG versions.
 /*@{*/
 
 
+#ifndef GL_FRAMEBUFFER
+#  define GL_FRAMEBUFFER 0x8D40
+#endif
+
+
 GLvoid OSGWTOOLS_EXPORT glGenFramebuffers( osg::FBOExtensions* fboExt, GLsizei n, GLuint* framebuffer );
 GLvoid OSGWTOOLS_EXPORT glDeleteFramebuffers( osg::FBOExtensions* fboExt, GLsizei n, GLuint* framebuffer );
 GLvoid OSGWTOOLS_EXPORT glBindFramebuffer( osg::FBOExtensions* fboExt, GLenum target, GLuint framebuffer );
 
 GLvoid OSGWTOOLS_EXPORT glFramebufferTexture2D( osg::FBOExtensions* fboExt, GLenum target, GLenum attachment,
             GLenum textarget, GLuint texture, GLint level );
+GLvoid OSGWTOOLS_EXPORT glBlitFramebuffer(  osg::FBOExtensions* fboExt, GLint srcX0, GLint srcY0, GLint srcX1,
+            GLint srcY1, GLint dstX0, GLint dstY0, GLint dstX1, GLint dstY1, GLbitfield mask, GLenum filter );
+
 
 
 /*@}*/
