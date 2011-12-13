31eed9287fe53f9155089812121df3ca9a35bdb4
xenonofarcticus@gmail.com <xenonofarcticus@gmail.com@c67418bc-a6eb-11de-8ba5-91705c1537ed>
2011-12-13 22:42:38 +0000

    Implement SpaceBall and SpaceBallVRPN support.

--- trunk/examples/MxMovement/MxMovementExample.cpp
+++ trunk/examples/MxMovement/MxMovementExample.cpp
@@ -5,8 +5,13 @@
 #include <osgViewer/Viewer>
 #include <osgViewer/ViewerEventHandlers>
 
-#include <osgwMx/MxGamePadDX.h>
 #include <osgwMx/MxCore.h>
+#ifdef DIRECTX_ENABLED
+#include <osgwMx/MxGamePadDX.h>
+#endif // DIRECTX_ENABLED
+#ifdef VRPN_ENABLED
+#include <osgwMx/MxSpaceBallVRPN.h>
+#endif // VRPN_ENABLED
 
 
 using namespace osgwMx;
@@ -15,7 +20,7 @@ int main( int argc, char** argv )
 {
     osg::ArgumentParser arguments( &argc, argv );
 
-    double moveRate( 1. );
+	double moveRate( 1. );
     arguments.read( "--rate", moveRate );
 
     std::string mapFile( "" );
@@ -37,24 +42,35 @@ int main( int argc, char** argv )
     viewer.setSceneData( root.get() );
     viewer.addEventHandler( new osgViewer::StatsHandler );
 
-    // create a game pad input handler and data interpreter to control the view.
-    osg::ref_ptr< MxGamePadDX > gamePad = new MxGamePadDX;
-    gamePad->setStickRate( moveRate );
+#ifdef DIRECTX_ENABLED
+	// create a game pad input handler and data interpreter to control the view.
+    osg::ref_ptr< MxGamePadDX > mxController = new MxGamePadDX;
+    mxController->setStickRate( moveRate );
 
-    // Load the optional gamepad functional mapping, if specified.
-    if( !( mapFile.empty() ) )
-    {
-        osgwMx::FunctionalMap* map = dynamic_cast< osgwMx::FunctionalMap* >(
-            osgDB::readObjectFile( mapFile ) );
-        if( map != NULL )
-            gamePad->setFunctionalMap( map );
-        else
-            osg::notify( osg::WARN ) << "Unable to load map file \"" << mapFile << "\"" << std::endl;
-    }
+#elif VRPN_ENABLED
+	moveRate *= 10.0; // Spaceball, even with normalized range, seems very sluggish
+	double rotRate( 30. );
+	// create a spaceball input handler and data interpreter to control the view.
+	osg::ref_ptr< MxSpaceBallVRPN > mxController = new MxSpaceBallVRPN;
+	mxController->setAllRates( moveRate, rotRate );
 
-    // Set some MxCore defaults:
-    osgwMx::MxCore* mxCore = gamePad->getMxCore();
-    mxCore->setInitialValues( osg::Vec3d( 0., 0., 1. ), osg::Vec3d( 0., 1., 0. ),
+#endif // VRPN_ENABLED
+
+	// Load the optional functional mapping, if specified.
+	if( !( mapFile.empty() ) )
+	{
+		osgwMx::FunctionalMap* map = dynamic_cast< osgwMx::FunctionalMap* >(
+			osgDB::readObjectFile( mapFile ) );
+		if( map != NULL )
+			mxController->setFunctionalMap( map );
+		else
+			osg::notify( osg::WARN ) << "Unable to load map file \"" << mapFile << "\"" << std::endl;
+	}
+
+	// Set some MxCore defaults:
+	osgwMx::MxCore* mxCore = mxController->getMxCore();
+
+	mxCore->setInitialValues( osg::Vec3d( 0., 0., 1. ), osg::Vec3d( 0., 1., 0. ),
         osg::Vec3d( 0., -30., 0. ) );
     mxCore->reset();
 
@@ -69,7 +85,7 @@ int main( int argc, char** argv )
     {
         // DX uses polling; other VR device SDKs might send events.
         const double currSimTime = viewer.getFrameStamp()->getSimulationTime();
-        gamePad->poll( currSimTime - prevSimTime );
+        mxController->poll( currSimTime - prevSimTime );
         prevSimTime = currSimTime;
 
         // Update the current view matrix.
