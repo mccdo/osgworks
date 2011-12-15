65237b19af76e4cb6f781ab6f960c39ceaf7aa10
SkewMatrix@gmail.com <SkewMatrix@gmail.com@c67418bc-a6eb-11de-8ba5-91705c1537ed>
2011-12-15 22:05:09 +0000

    Fix for issue 13, patch from kcbale, plus additional changes by me. "--allNodes" sets the override mask so users can see subgraphs of nodes that have a 0 node mask. Also, "--mask <x>" sets the traversal mask so you can filter what parts of the scene graph you want to view based on their node masks.

--- apps/osgwnames/osgwnames.cpp
+++ apps/osgwnames/osgwnames.cpp
@@ -87,6 +87,8 @@ main( int argc,
 
     arguments.getApplicationUsage()->addCommandLineOption( "-O <option>", "Pass <option> to import plugin." );
     arguments.getApplicationUsage()->addCommandLineOption( "--nogeodes", "Do not display Geodes in the output." );
+    arguments.getApplicationUsage()->addCommandLineOption( "--mask <x>", "Traversal mask. <x> is up to 8 hex digits. Default is \"ffffffff\"." );
+    arguments.getApplicationUsage()->addCommandLineOption( "--allNodes", "Traverse all nodes regardless of nodemask." );
     arguments.getApplicationUsage()->addCommandLineOption( "-v/--version", "Display the osgWorks version string." );
 
     if( arguments.read( "-h" ) || arguments.read( "--help" ) )
@@ -97,8 +99,32 @@ main( int argc,
     }
 
     bool showGeodes( true );
-    if( arguments.find( "--nogeodes" ) >= 0 )
+    if( arguments.read( "--nogeodes" ) )
         showGeodes = false;
+
+    unsigned int mask( 0xffffffff );
+    int maskPos( 0 );
+    if( ( maskPos = arguments.find( "--mask" ) ) > 0 )
+    {
+        bool failed( true );
+        if( arguments.argc() > maskPos+1 )
+        {
+            std::istringstream istr( arguments[ maskPos+1] );
+            istr >> std::hex >> mask;
+            failed = istr.fail();
+        }
+        if( failed )
+        {
+            osg::notify( osg::WARN ) << "--mask must be followed by a hex number." << std::endl;
+            mask = 0xffffffff;
+        }
+        else
+        {
+            arguments.remove( maskPos, 2 );
+        }
+    }
+
+    const bool allNodes( arguments.read( "--allNodes" ) );
 
     std::string str;
     while( arguments.read( "-O", str ) )
@@ -109,9 +135,7 @@ main( int argc,
     }
 
     if( arguments.read( "-v" ) || arguments.read( "--version" ) )
-    {
         osg::notify( osg::ALWAYS ) << osgwTools::getVersionString() << std::endl << std::endl;
-    }
 
 
     osg::ref_ptr< osg::Node > root = osgDB::readNodeFiles( arguments );
@@ -124,6 +148,10 @@ main( int argc,
 
 
     ShowNodeNamesVisitor snnv( showGeodes );
+    snnv.setTraversalMask( mask );
+    if( allNodes )
+        snnv.setNodeMaskOverride( 0xffffffff );
+
     root->accept( snnv );
 
     return( 0 );
@@ -138,19 +166,19 @@ represented as a single line of text, containing the class and Node name.
 If the Node name is empty, \e NULL displays in its place.
 
 \section su Simple Usage
-Try running osgwnames on \c dectest20.osg, one of the osgWorks data files:
+Try running osgwnames on \c dectest20.ive, one of the osgWorks data files:
 
 \code
 C:\Projects>osgwnames dectest20.ive
 world (Group)
   door_assembly.asm.2 (Group)
     NULL (MatrixTransform)
-      KNOB.PRT (Geode)
+      KNOB.PRT (Geode with 1 Drawable)
     NULL (MatrixTransform)
-      KNOB.PRT #2 (Geode)
+      KNOB.PRT #2 (Geode with 1 Drawable)
     NULL (MatrixTransform)
-      DOOR.PRT (Geode)
-    POLE.PRT (Geode)
+      DOOR.PRT (Geode with 1 Drawable)
+    POLE.PRT (Geode with 1 Drawable)\endcode
 \endcode
 
 The output shows the scene graph hierarchical structure:
@@ -178,6 +206,14 @@ world (Group)
   <tr>
     <td><b>--nogeodes</b></td>
     <td>Do not display Geodes in the output.</td>
+  </tr>
+  <tr>
+    <td><b>--allNodes</b></td>
+    <td>Traverse all nodes regardless of node mask.</td>
+  </tr>
+  <tr>
+    <td><b>--mask <x></b></td>
+    <td>Specify the scene graph traversal mask. Replace <x> with up to 8 hex digits. Default is ffffffff.</td>
   </tr>
   <tr>
     <td><b>-O <option></b></td>
